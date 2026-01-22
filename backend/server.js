b57e9e06527a0fd4e9f1fd697509d1fdfde661e2
Adrián.Álvarez <adrian.alvarez@eusa.es>
2026-01-22 13:30:22 +0100

    second change

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI).then(() =>
+  console.log('Connected to MongoDB')).catc
